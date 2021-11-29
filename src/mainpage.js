2bb1188610c6bd46ea004353d792b4cc8672ac8f
Dorgee Lama <dorgeelama@Dorgees-MacBook-Pro.local>
2021-11-29 12:23:18 -0700

    Crypto tracker done

---
+++ src/mainpage.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import React from 'react';
+import Coin from './coin';
+
+export const MainPage = () => {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) => {
+    return coin.name.toLowerCase().includes(search.toLocaleLowerCase());
+  });
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <div className='coin-text'>Search for a currency</div>
+        <form>
+          <input
+            type='text'
+            placeholder='Search'
+            className='coin-input'
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
+    </div>
+  );
+};
