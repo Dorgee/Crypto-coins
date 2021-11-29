ec6a5aba4e0f626d0e1725468190069eaa0406fd
Dorgee Lama <dorgeelama@Dorgees-MacBook-Pro.local>
2021-11-29 12:30:04 -0700

    added some labels

--- src/coin.js
+++ src/coin.js
@@ -18,12 +18,16 @@ const coin = ({
           <p className='coin-symbol'>{symbol}</p>
         </div>
         <div className='coin-data'>
-          <p className='coin-price'>${price}</p>
-          <p className='coin-volume'>${volume.toLocaleString()}</p>
+          <p className='coin-price'>Price: ${price}</p>
+          <p className='coin-volume'>Volume: ${volume.toLocaleString()}</p>
           {priceChange < 0 ? (
-            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+            <p className='coin-percent red'>
+              Change: {priceChange.toFixed(2)}%
+            </p>
           ) : (
-            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+            <p className='coin-percent green'>
+              Change: {priceChange.toFixed(2)}%
+            </p>
           )}
           <p className='coin-marketcap'>
             Mkt Cap: ${marketcap.toLocaleString()}
